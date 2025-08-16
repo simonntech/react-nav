6b9bb56831540c283506a67eab03a9292655fdc7
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-16 10:54:04 -0300

    routes init

---
+++ routes/stack.routes.tsx
@@ -0,0 +1,15 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from '../src/screens/HomeScreen';
+import GalleryScreen from '../src/screens/GalleryScreen';
+
+const {Screen, Navigator} = createStackNavigator();
+
+export function StackRoutes() {
+    return (
+        <Navigator>
+            <Screen 
+                
+            />
+        </Navigator>
+    )
+}
