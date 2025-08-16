01b4bcc8b20f8efcdbfac08ac6983971a41c13d8
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-16 11:15:27 -0300

    nav button

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import {View, Text} from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }: any) {
+
+    function navToGallery() {
+        navigation.navigate('gallery')
+    }
+
     return (
-        <View style={{flex: 1, backgroundColor: 'blue'}}>
-            <Text style={{color: 'white', fontSize: 24}}>Home works</Text>
+        <View style={{ flex: 1, backgroundColor: 'blue' }}>
+            <Text style={{ color: 'white', fontSize: 24 }}>Home works</Text>
+            <Button
+                title='Go to Gallery'
+                onPress={navToGallery}
+            />
             <StatusBar style='auto'></StatusBar>
         </View>
     )
