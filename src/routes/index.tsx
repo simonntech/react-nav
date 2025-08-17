8ca3f9250afbbceb3f8ad5ba7ffe919c22e49612
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-17 09:49:18 -0300

    drawer routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StackRoutes } from './stack.routes';
 import { BottomTabsRoutes } from './bottom-tabs.routes';
+import { DrawerRoutes } from './drawer.routes';
 
 export function Routes() {
     return (
         <NavigationContainer>
-            <BottomTabsRoutes />
+            <DrawerRoutes />
         </NavigationContainer>
     )
 
